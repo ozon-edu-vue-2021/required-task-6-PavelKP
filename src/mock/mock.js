8f4e1b84ae56861e917244b760ba9ada2ea3ee49
PavelKP <mr.pavel.krasnov@yandex.ru>
2021-11-28 19:06:43 +0300

    replace folders

---
+++ src/mock/mock.js
@@ -0,0 +1,34 @@
+export const rows = [
+	{
+		id: 1,
+		name: 'Сыр "Российский"',
+		description: "Сыр из отбороного сухого молока",
+		price: 150,
+		email: "Eliseo@gardner.biz",
+		image: "https://picsum.photos/id/1/100",
+	},
+	{
+		id: 2,
+		name: "Молоко 3,2%",
+		description: "Из сухого молока от лучших коров подмосковья",
+		price: 65,
+		email: "wdwd@gardner.biz",
+		image: "https://picsum.photos/id/2/100",
+	},
+	{
+		id: 3,
+		name: "Творог 5%",
+		description: "Из сухого молока от лучших коров подмосковья",
+		price: 125,
+		email: "tvorog@gardner.biz",
+		image: "https://picsum.photos/id/3/100",
+	},
+	{
+		id: 4,
+		name: "Сырок Б.Ю. Александров",
+		description: "Самый дорогой сырок в магазине",
+		price: 100,
+		email: "tvorog@gardner.biz",
+		image: "https://picsum.photos/id/4/100",
+	},
+];
